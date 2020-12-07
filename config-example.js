5fa41c2dc9e1f86ee50a77925635f399e160a54c
Benjamin <nimajneb2005@gmx.de>
2020-12-07 15:00:58 +0100

    Add example for a config.js file

---
+++ config-example.js
@@ -0,0 +1,13 @@
+const config = {
+    db      : {
+        host: 'SQL-Server adress',
+        user: 'SQL-Server user',
+        password: 'Password for the user',
+        database: 'Name of the database'
+    },
+
+    port    : 3000
+}
+
+
+exports.config = config
