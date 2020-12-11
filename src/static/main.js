ee28b9b8425bcd7bdd2c12591c1b7e6ec4865cbc
Benjamin <nimajneb2005@gmx.de>
2020-12-11 20:14:40 +0100

    Add spinner while loading poll data

--- src/static/main.js
+++ src/static/main.js
@@ -1,12 +1,33 @@
 class Poll {
-    constructor(id) {
-        this.id = id
+    constructor() {
+        this.spinnerContainer = document.getElementById('spinner-container')
+        this.content = document.getElementById('content')
+    }
+
+    getId() {
+        /* Gets the id from the URL */
+        let path = window.location.pathname
+        path = path.split('/')
+        this.id = Number(path[2])
+    }
+
+    setSpinner(spinner = Boolean) {
+        /* Displays the Spinner if `spinner` is true and displays if it's false */
+        if (spinner) {
+            this.spinnerContainer.style.display = 'block'
+            this.content.style.display = 'none'
+        } else {
+            this.spinnerContainer.style.display = 'none'
+            this.content.style.display = 'block'
+        }
     }
 
     async fetchData() {
         await fetch(`../api/poll/${this.id}`)
         .then( resp => resp.json() )
         .then( data => this.data = data )
+
+        this.setSpinner(false)
 
         console.log(this.data);
     }
@@ -17,6 +38,7 @@ class chart {
     constructor(data) {
         this.data = data
         this.el_bar = document.getElementById('bar-chart')
+        this.el_pie = document.getElementById('pie-chart')
     }
 
     async drawLineDiagramm() {
@@ -24,11 +46,9 @@ class chart {
     }
 }
 
-
-let id = 1
-
-poll = new Poll(id)
+const poll = new Poll()
 
 document.body.onload = () => {
+    poll.getId()
     poll.fetchData()
 }
