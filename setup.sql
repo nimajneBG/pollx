82831e28b76e7f6d83200ed2a8ebfd37f7638ced
Benjamin <nimajneb2005@gmx.de>
2021-01-20 17:26:05 +0100

    Change db struture for results

--- setup.sql
+++ setup.sql
@@ -8,25 +8,37 @@ USE `pollx`;
 
 DROP TABLE IF EXISTS `polls`;
 CREATE TABLE `polls` (
-  `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id` int(11) NOT NULL,
-  `public` tinyint(1) NOT NULL,
-  `answers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`answers`)),
-  `email` text COLLATE utf8mb4_unicode_ci NOT NULL
+    `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `id` int(11) NOT NULL,
+    `public` tinyint(1) NOT NULL,
+    `answers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`answers`)),
+    `email` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `results`;
 CREATE TABLE `results` (
-  `poll_id` int(11) NOT NULL,
-  `selected_option` int(11) NOT NULL
+    `poll_id` int(11) NOT NULL,
+    `opt0` int(11) NOT NULL,
+    `opt1` int(11),
+    `opt2` int(11),
+    `opt3` int(11),
+    `opt4` int(11),
+    `opt5` int(11),
+    `opt6` int(11),
+    `opt7` int(11),
+    `opt8` int(11),
+    `opt9` int(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `polls`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `results`
+    ADD PRIMARY KEY (`poll_id`);
 
 ALTER TABLE `polls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 
 COMMIT;
