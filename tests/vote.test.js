f43e9b9ab3c370217cb41f13336b64ac3e16d21c
Benjamin <nimajneb2005@gmx.de>
2021-04-01 11:33:46 +0200

    Write test for the vote api

---
+++ tests/vote.test.js
@@ -0,0 +1,46 @@
+/* 
+    Unit Test for the vote function in the api
+
+    1 Checks Vote that should work
+
+    2 Check Vote that should not work (Invalid request)
+
+    3 Check Vote that should not work (Already voted)
+
+    Important for this test is that the ids and options in the variables exist.
+    Moreover it should be noted that the results on them will be influenced by 
+    this tests
+*/
+
+const request = require('supertest')
+const app = require('../app')
+
+const agent = request.agent(app)
+
+const existingIds = [10, 11]
+const existingOption = 1
+
+
+test('Should be able vote on a poll', async () => {
+    await agent
+        .post(`/api/vote/${existingIds[0]}`)
+        .send({ "option": existingOption })
+        .expect(200)
+})
+
+
+test('Should not be able to vote (invalid request)', async () => {
+    await agent
+        .post(`/api/vote/${existingIds[1]}`)
+        .send({ "invalid field": 1 })
+        .expect(400)
+})
+
+test('Should not be able to vote on a poll (already voted)', async () => {
+    await agent
+        .post(`/api/vote/${existingIds[0]}`)
+        .send({ "option": existingOption })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect('Content-Length', "36")
+})
