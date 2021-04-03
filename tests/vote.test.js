f84eeee5b8fdc844c7d534919270cc90cdce1d85
Benjamin <nimajneb2005@gmx.de>
2021-04-04 00:09:05 +0200

    Use describe

--- tests/vote.test.js
+++ tests/vote.test.js
@@ -20,27 +20,28 @@ const agent = request.agent(app)
 const existingIds = [10, 11]
 const existingOption = 1
 
+describe('Test the vote api endpoint', () => {
+    test('Should be able vote on a poll', async () => {
+        await agent
+            .post(`/api/vote/${existingIds[0]}`)
+            .send({ "option": existingOption })
+            .expect(200)
+            .expect('Content-Length', "2")
+    })
 
-test('Should be able vote on a poll', async () => {
-    await agent
-        .post(`/api/vote/${existingIds[0]}`)
-        .send({ "option": existingOption })
-        .expect(200)
-})
+    test('Should not be able to vote (invalid request)', async () => {
+        await agent
+            .post(`/api/vote/${existingIds[1]}`)
+            .send({ "invalid field": 1 })
+            .expect(400)
+    })
 
-
-test('Should not be able to vote (invalid request)', async () => {
-    await agent
-        .post(`/api/vote/${existingIds[1]}`)
-        .send({ "invalid field": 1 })
-        .expect(400)
-})
-
-test('Should not be able to vote on a poll (already voted)', async () => {
-    await agent
-        .post(`/api/vote/${existingIds[0]}`)
-        .send({ "option": existingOption })
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect('Content-Length', "36")
+    test('Should not be able to vote on a poll (already voted)', async () => {
+        await agent
+            .post(`/api/vote/${existingIds[0]}`)
+            .send({ "option": existingOption })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('Content-Length', "36")
+    })
 })
